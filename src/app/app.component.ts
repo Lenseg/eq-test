cf80cdc0f0824b9cf9435696481309aa3c55d0a1
Антон Степаненков <lens@MacBook-Air-Anton-2.local>
2021-04-20 20:36:02 +0300

    removed api dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -140,7 +140,7 @@ export class AppComponent {
     })
   }
   getServices() {
-    this.dataServices.getServices()
-      .subscribe((services:[]) => this.services = services);
+    this.services = this.dataServices.getServices();
+      // .subscribe((services:[]) => this.services = services);
   }
 }
